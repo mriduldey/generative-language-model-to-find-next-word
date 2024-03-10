696320cd25afa61eb4e0770272b0a232533b832a
Mridul Dey <mdey@infinera.com>
2024-03-10 21:07:50 +0530

    initial commit

---
+++ 01/01_02_begin.py
@@ -0,0 +1,16 @@
+import random
+from collections import defaultdict
+
+tokens = ["I", "try", "to", "learn", "something", "new", "every", "day"]
+
+graph = defaultdict(list)
+
+for i, token in enumerate(tokens):
+    print(i, token)
+
+print(random.choice(tokens))
+print(random.choice(tokens))
+
+print(random.choice(tokens))
+print(random.choice(tokens))
+print(random.choice(tokens))
